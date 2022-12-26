868c6a6079d89d93406de4cc0770d240b79d424a
Ilham Dohaan <doha39anilham@gmail.com>
2022-12-26 11:01:26 +0700

    change pdf

--- src/util/pdfkit.js
+++ src/util/pdfkit.js
@@ -1,30 +1,39 @@
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
-const {insertFileNameData} = require('../model/voucher-data');
+const { insertFileNameData } = require('../model/voucher-data');
 
-const generatePdf = (voucherCode, emailId) =>{
-    return new Promise(async(resolve, reject) =>{
-        try{
-            const doc = new PDFDocument({size: 'A7', layout: 'landscape', margin:0})
-            doc.pipe(fs.createWriteStream(__dirname+`../../../storage/voucherpdf/${voucherCode}.pdf`))
-            doc
-               .image(fs.readFileSync(__dirname+'../../assets/wave.jpg'),{width: 297.64, height: 100})
-                .fontSize(16)
-                .text('Voucher Kamu',{
-                    align: 'center'
+const generatePdf = (voucherCode, emailId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            const pdfFile = muhammara.createWriterToModify(
+                __dirname + '../assets/designVoucher.pdf', {
+                modifiedFilePath: __dirname + "../../../storage/voucherpdf/${voucherCode}.pdf",
+            }
+            );
+
+            const pageModifier = new muhammara.PDFPageModifier(pdfFile, 0);
+            pageModifier
+                .startContext()
+                .getContext()
+                .writeText(voucherCode, 300, 46, {
+                    font: pdfFile.getFontForFile(
+                        __dirname + "/robotto.ttf"
+                    ),
+                    size: 25,
+                    colorspace: "black",
+                    color: 0x00,
                 })
-                .text(voucherCode,{
-                    align: 'center'
-                })
-                .end();
-                console.log('success create pdf '+voucherCode);
-                await insertFileNameData(voucherCode+'.pdf', emailId);
-                resolve(true);
-        }catch(err){
+            pageModifier.endContext().writePage();
+            pdfFile.end();
+            console.log('success create pdf ' + voucherCode);
+            await insertFileNameData(voucherCode + '.pdf', emailId);
+            resolve(true);
+        } catch (err) {
             console.log('failed create pdf');
             reject(`generatePdf\n${err}`);
         }
-    }); 
+    });
 }
 
 module.exports = generatePdf;
